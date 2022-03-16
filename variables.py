21000bd3159415cce857eb84e9e1600accad1f71
123echo321 <101233845+123echo321@users.noreply.github.com>
2022-03-16 19:39:01 +0800

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,1 +1,23 @@
-
+-bash-4.2$ python
+Python 2.7.5 (default, Nov 16 2020, 22:23:17)
+[GCC 4.8.5 20150623 (Red Hat 4.8.5-44)] on linux2
+Type "help", "copyright", "credits" or "license" for more information.
+>>> a=19245301
+>>> b=4218520
+>>> c=271
+>>> d=b-c
+>>> e=a-b
+>>> d!=e
+True
+>>> d>e
+False
+>>> d<e
+True
+>>> X = True
+>>> Y = False
+>>> W=X and Y
+>>> print(W)
+False
+>>> Q=X or Y
+>>> print(Q)
+True
