09db0b0ce926e1d4b9e67965be0d59523b36ec4f
123echo321 <101233845+123echo321@users.noreply.github.com>
2022-05-22 13:09:48 +0800

    Create variables.py

---
+++ practical5/variables.py
@@ -0,0 +1,28 @@
+a = 19245301  # 2022
+b = 4218520  # 2021
+c = 271  # 2020
+d = b-c
+e = a-b
+if d > e:
+    print("d is greater")
+if d < e:
+    print("e is greater")
+f = e/b  # rate  of new cases in 2021
+g = d/c  # rate  of new cases in 2020
+if f > g:
+    print("rate of new cases in 2021 is greater")
+if f < g:
+    print("rate of new cases in 2020 is greater")
+
+
+X = 0
+Y = 1
+bool(X)
+bool(Y)
+print("X:",bool(X))
+print("Y:",bool(Y))
+
+
+
+Z = bool(X) and bool(Y)
+print("X and Y:",Z)
